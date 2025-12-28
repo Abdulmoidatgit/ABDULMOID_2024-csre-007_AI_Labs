abc18c732a22e0f9731b0e0dc44d2c0cc03d478d
Abdul moid <aloogamerz068@gmail.com>
2025-12-29 00:16:00 +0500

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,7 @@
+def greet(name):
+ """A simple function to greet someone."""
+ return f"Hello, {name}! Welcome to Week 8."
+def add_numbers(a, b):
+ """Adds two numbers and returns the result."""
+ return a + b
+  
